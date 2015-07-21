6ac296548afcf9f5885c504ecf2b3b5f8107b793
Haarieh <suporte@haarieh.com>
2015-07-21 08:30:28 -0300

    Ajuste final

--- igarape.php
+++ igarape.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
  * Plugin Name: Igarapé Adicionais Plugin
  * Plugin URI: http://www.igarape.org.br
@@ -7,20 +7,46 @@
  * Author: Anderson Silva
  * Author URI: http://www.haarieh.com/
 */
-function pre_option_qode_options_stockholm($qode_options_stockholm) {
-    print_r($qode_options_stockholm)
-    /*$en = "http://igarape.org.br/wp-content/themes/stockholm/en.jpg";
+function function_qode_options_stockholm($qode_options_stockholm) {
+    $qode_options = $qode_options_stockholm;
+    $en = "http://igarape.org.br/wp-content/themes/stockholm/en.jpg";
     $pt = "http://igarape.org.br/wp-content/themes/stockholm/pt.png";
-    if(ICL_LANGUAGE_CODE == "en") {    	
-    	$qode_options['logo_image'] = $en;
-    	$qode_options['logo_image_light'] = $en;
-    	$qode_options['logo_image_dark'] = $en;
+    if(ICL_LANGUAGE_CODE == "en") {
+        $qode_options['logo_image'] = $en;
+        $qode_options['logo_image_light'] = $en;
+        $qode_options['logo_image_dark'] = $en;
     } else {
-    	$qode_options['logo_image'] = $pt;
-    	$qode_options['logo_image_light'] = $pt;
-    	$qode_options['logo_image_dark'] = $pt;
-    }*/
+        $qode_options['logo_image'] = $pt;
+        $qode_options['logo_image_light'] = $pt;
+        $qode_options['logo_image_dark'] = $pt;
+    }
+    return $qode_options;
 }
-add_filter('pre_option_posts_per_page', 'limit_posts_per_page');
+add_filter('option_qode_options_stockholm', 'function_qode_options_stockholm');
 ?>
-
+<?php
+/*
+ * Plugin Name: Igarapé Adicionais Plugin
+ * Plugin URI: http://www.igarape.org.br
+ * Description: Igarapé Adicionais Plugin
+ * Version: 1.0
+ * Author: Anderson Silva
+ * Author URI: http://www.haarieh.com/
+*/
+function function_qode_options_stockholm($qode_options_stockholm) {
+    $qode_options = $qode_options_stockholm;
+    $en = "http://igarape.org.br/wp-content/themes/stockholm/en.jpg";
+    $pt = "http://igarape.org.br/wp-content/themes/stockholm/pt.png";
+    if(ICL_LANGUAGE_CODE == "en") {
+        $qode_options['logo_image'] = $en;
+        $qode_options['logo_image_light'] = $en;
+        $qode_options['logo_image_dark'] = $en;
+    } else {
+        $qode_options['logo_image'] = $pt;
+        $qode_options['logo_image_light'] = $pt;
+        $qode_options['logo_image_dark'] = $pt;
+    }
+    return $qode_options;
+}
+add_filter('option_qode_options_stockholm', 'function_qode_options_stockholm');
+?>
